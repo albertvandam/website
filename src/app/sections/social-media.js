b6c5d0fd8a62916604081720af3caff04ac17178
Albert van Dam <albertvd@gmail.com>
2017-03-17 21:49:39 +0200

    Fix contact link

--- src/app/sections/social-media.js
+++ src/app/sections/social-media.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const IMAGE_URL = process.env.IMAGE_URL;
 
 export default function () {
     return (
         <div className="socialMedia">
-            <a href="#contact"><i className="material-icons email">email</i></a>
+            <Link to="/contact"><i className="material-icons email">email</i></Link>
 
-            <a rel="noopener" href="https://www.linkedin.com/in/avandamza/" target="_blank">
-                <img src={IMAGE_URL + '/linkedin.svg'} alt="LinkedIn" width={18} height={18}/>
-            </a>
+            <a rel="noopener" href="https://www.linkedin.com/in/avandamza/" target="_blank"> <img
+                src={IMAGE_URL + '/linkedin.svg'} alt="LinkedIn" width={18} height={18}/> </a>
 
-            <a rel="noopener" href="https://github.com/albertvandam?tab=repositories" target="_blank">
-                <img  src={IMAGE_URL + '/github-box.svg'} alt="GitHub" width={18} height={18}/>
-            </a>
+            <a rel="noopener" href="https://github.com/albertvandam?tab=repositories" target="_blank"> <img
+                src={IMAGE_URL + '/github-box.svg'} alt="GitHub" width={18} height={18}/> </a>
         </div>
     );
 }
